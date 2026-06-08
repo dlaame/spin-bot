c9c94f620168e8ab5612ff3ddf86f2fe22685a4d
Delayne <dlame@Delaynes-MacBook-Pro.local>
2026-06-08 13:47:17 -0500

    global commands

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   await rest.put(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    Routes.applicationCommands(process.env.CLIENT_ID),
     { body: commands },
   );
 })();
